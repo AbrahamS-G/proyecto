f373ad2407fe9a2c4df5c90a23a4e1522d64cf13
Abraham SG. <abraham@dev.com>
2026-03-16 20:46:11 -0600

    Starting diseño terminado, falta logica y crear el form register, se muestra correctamente el loader(forzandolo)

--- inc/menu.php
+++ inc/menu.php
@@ -2,7 +2,7 @@
     <ul>
         <li onclick="cargarPagina('./inicio',true,'Inicio')"><img src="./assets/img/default/inicio.svg" alt="">Inicio</li>
         <li onclick="cargarPagina('./dashboard',true,'Dashboard')"><img src="./assets/img/default/dashboard.svg" alt="">Dashboard</li>
-        <!-- <li onclick="cargarPagina('./starting',true,'Starting')"><img src="./assets/img/default/starting.svg" alt="">Starting</li> -->
+        <li onclick="cargarPagina('./cuenta',true,'Cuenta')"><img src="./assets/img/default/cuenta.svg" alt="">Cuenta</li>
         <li onclick="cargarPagina('./info',true,'Información')"><img src="./assets/img/default/info.svg" alt="">Información</li>
         <li onclick="cargarPagina('./laboratorio',true,'Laboratorio')"><img src="./assets/img/default/lab.svg" alt="">Laboratorio</li>
         <li onclick="cargarPagina('./logout',true,'Cerrar Sesión')"><img src="./assets/img/default/logout.svg" alt="">Cerrar Sesión</li>
