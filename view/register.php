fe918fcbd9f6bc120d6e83a991fa4bae798d7add
Abraham SG. <abraham@dev.com>
2026-04-08 20:12:22 -0600

    Avance en el registro

---
+++ view/register.php
@@ -0,0 +1,10 @@
+<div class="creadorUrl">
+    <b id="mensajeUrl"></b>
+    <form action="/proyecto/register" method="post">
+        <h2>Crear una cuenta</h2>
+        <input type="text" name="nombre" placeholder="Ingresa el nombre de usuario" id="NombreUsuario" autocomplete="off">
+        <input type="email" name="correo" placeholder="Ingresa el correo electronico" id="Correo" autocomplete="off">
+        <input type="password" name="password" placeholder="Ingresa la contraseña" id="Password" autocomplete="off">
+        <button class="btn-menu" type="button">Crear</button>
+    </form>
+</div>
