09372d402447df8c4a4bb87cf492da82e094912c
Abraham SG. <abraham@dev.com>
2026-04-08 19:57:42 -0600

    Proyecto avanza a la versión BETA 0.0.3.2

--- core/Bootstrap.php
+++ core/Bootstrap.php
@@ -2,7 +2,7 @@
 class Bootstrap{
     public static function init(){
         date_default_timezone_set('America/Mexico_City');
-        define("PROYECTO_VERSION", "BETA 0.0.3.1");
+        define("PROYECTO_VERSION", "BETA 0.0.3.2");
         // nombre de la sesion
         session_name("PROYECTO");
 
